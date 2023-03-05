a931bfeb15dd2e36026523a54854edeb512dd749
AshutoshVohra136 <113580146+AshutoshVohra136@users.noreply.github.com>
2023-03-05 20:10:25 +0530

    Added Fibonacci code

--- Lecture032 Recursion Day2/Fibbonacci.cpp
+++ Lecture032 Recursion Day2/Fibbonacci.cpp
@@ -1,11 +1,8 @@
 #include<iostream> 
 using namespace std;
 
-
-int main() {
-
-
-
-
-    return 0;
+int fibonacci(int n){
+    if (n==0 || n==1)return n;
+    return fibonacci(n-1)+fibonacci(n-2);
+    
 }
